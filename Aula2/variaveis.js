65498c81ff2cb870353ff8700f1cf0cc45eff218
MiguelDVigano <miguel.ditanno.vigano.1.3@gmail.com>
2026-04-22 13:19:25 -0400

    fazendo o exercicio da aula 2 de JS - definindo variaveis de diferentes tipos e exibindo no terminal

---
+++ Aula2/variaveis.js
@@ -0,0 +1,9 @@
+const nome = "Miguel Di-Tanno Viganó";
+const idade = 17;
+const matricula = true;
+const cidade = "Araras";
+
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Matricula: " + matricula);
+console.log("Cidade: " + cidade);
